e9aac31c9d1374e6361ad1d9e5b25e0081d89422
YoungKim <kimyk0120@hotmail.com>
2018-07-13 14:14:30 +0900

    Change-Id: I18d4b6758b5e3f4a6657d6787d4cf6672d98b563 Signed-off-by: YoungKim <kimyk0120@hotmail.com>

--- ExamJavaScript2018/blockchain/blockchain.js
+++ ExamJavaScript2018/blockchain/blockchain.js
@@ -1,3 +1,53 @@
+
+
+
+console.log("blockchain.js file called");
+
+
+
+//블록체인 데이터 구조
+function Blockchain(){
+	this.chain = [];
+	this.newTransactions = [];
+}
+
+
+//블록체인 프로토 타입 함수 정의
+Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
+	//새 블록 객체
+	const newBlock = {
+	index: this.chain.length + 1,
+	timestamp: Date.now(),
+	transactions: this.newTransactions,
+	nonce:nonce,
+	hash:hash,
+	previousBlockHash:previousBlockHash
+};
+
+	//다음 거래를 위한 거래내역 배열 비워주고 새로운 블록을 chin 배열에 추가
+	this.newTransactions = [];
+	this.chain.push(newBlock);
+	
+	return newBlock;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**
- * http://usejsdoc.org/
+ * 출처: http://abc1211.tistory.com/516?category=1003529 [길위의 개발자]
  */
+
+
+
+
+
--- ExamJavaScript2018/blockchain/test.js
+++ ExamJavaScript2018/blockchain/test.js
@@ -3,3 +3,18 @@
  */
 
 console.log("test file called");
+
+
+function User(name,age){
+	this.name = name;
+	this.age = age;
+}
+User.prototype.emailDomain = "@google.co.kr";
+User.prototype.getEmailAddress = function(){
+	return this.name + this.age + this.emailDomain;
+}
+
+
+var user1 = new User('길위의 개발자', '30');
+var user50000 = new User('스티브잡스', '31');
+//console.log(user1.getEmailAddress());
