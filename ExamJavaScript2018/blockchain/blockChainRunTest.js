75a311eb93d3e4dfb26458bf460d6364dcce9042
YoungKim <kimyk0120@hotmail.com>
2018-07-24 11:21:01 +0900

    Signed-off-by: YoungKim <kimyk0120@hotmail.com>

--- ExamJavaScript2018/blockchain/blockChainRunTest.js
+++ ExamJavaScript2018/blockchain/blockChainRunTest.js
@@ -1,10 +1,12 @@
 /**
  * http://usejsdoc.org/
+ * 출처: http://abc1211.tistory.com/517?category=1003529 [길위의 개발자]
  */
 
-console.log("test file called");
+console.log("test.js file called");
 
-
+// prototype test
+/*
 function User(name,age){
 	this.name = name;
 	this.age = age;
@@ -13,8 +15,60 @@ User.prototype.emailDomain = "@google.co.kr";
 User.prototype.getEmailAddress = function(){
 	return this.name + this.age + this.emailDomain;
 }
-
-
 var user1 = new User('길위의 개발자', '30');
 var user50000 = new User('스티브잡스', '31');
-//console.log(user1.getEmailAddress());
+//console.log(user1.getEmailAddress());\
+*/
+
+
+// &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+
+
+
+//blockchain.js 모듈을 이곳에서 가져다 쓰겠다.
+const Blockchain = require('./blockchain');
+
+
+//위에서 가져온 모듈의 객체를 만든다.
+const bitcoin = new Blockchain();
+
+
+
+//새로운 블락 만들기
+bitcoin.createNewBlock(1111,"aaaaaaa","1a1a1a1a1a1a");
+
+//새로은 트랜잭션 생성 - (총금액, 보내는이, 받는이)
+bitcoin.createNewTransaction(100,'PACKadffaaf','HONGllalflks')
+
+
+
+
+
+//찍어보기
+console.log(bitcoin);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//endDOm
+
--- ExamJavaScript2018/blockchain/blockchain.js
+++ ExamJavaScript2018/blockchain/blockchain.js
@@ -1,14 +1,16 @@
+/*
+ * '블록 하나에  거래내역을 저장한다'
+ * 출처: http://abc1211.tistory.com/517?category=1003529 [길위의 개발자]
+ */
 
 
-
-console.log("blockchain.js file called");
-
+console.log("blockchain.js file called\n");
 
 
 //블록체인 데이터 구조
 function Blockchain(){
 	this.chain = [];
-	this.newTransactions = [];
+	this.pendingTransaction = [];
 }
 
 
@@ -16,37 +18,48 @@ function Blockchain(){
 Blockchain.prototype.createNewBlock = function(nonce,previousBlockHash,hash){
 	//새 블록 객체
 	const newBlock = {
-	index: this.chain.length + 1,
-	timestamp: Date.now(),
-	transactions: this.newTransactions,
-	nonce:nonce,
-	hash:hash,
-	previousBlockHash:previousBlockHash
-};
+		index: this.chain.length + 1,
+		timestamp: Date.now(),
+		transactions: this.pendingTransaction,
+		nonce:nonce,
+		hash:hash,
+		previousBlockHash:previousBlockHash
+	};
 
-	//다음 거래를 위한 거래내역 배열 비워주고 새로운 블록을 chin 배열에 추가
-	this.newTransactions = [];
+	//다음 거래를 위한 거래내역 배열 비워주고 새로운 블록을 chain 배열에 추가
+	this.pendingTransaction = [];
 	this.chain.push(newBlock);
 	
 	return newBlock;
 }
 
 
+//마지막 블록 얻기 - chain 배열에는 블록데이터가 들어간다. 맨마지막 블록을 가져와라.
+Blockchain.prototype.getLastBlock = function(){
+	return this.chain[this.chain.length - 1];
+}
+
+
+//새로운 트랜젝션(거래)가 발생했을 때 작동되는 함수
+//인자값으로, 총액수, 보내는사람, 받는사람이 들어간다.
+Blockchain.prototype.createNewTransaction = function(amount,sender,recipient){
+	const newTransaction = {
+		amount: amount,
+		sender: sender,
+		recipient: recipient
+	}
+
+	//맨위 트랜잭션 배열에 값을 넣어준다.
+	this.pendingTransaction.push(newTransaction);
+	
+	//마지막 블록의 index 에서 + 1
+	return this.getLastBlock()['index'] + 1
+}
 
 
 
-
-
-
-
-
-
-
-
-/**
- * 출처: http://abc1211.tistory.com/516?category=1003529 [길위의 개발자]
- */
-
+//Blockchain 모듈화
+module.exports = Blockchain;
 
 
 
